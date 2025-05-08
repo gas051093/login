975ef4f53ad7741e40ae4035fe83cdbed09a8f98
Gaston Magrassi <gas051093@hotmail.com.ar>
2025-05-08 20:23:06 -0300

    se aregaron js y json

--- js/main.js
+++ js/main.js
@@ -1,16 +1,38 @@
-const login = document.getElementById('login');
-login.addEventListener("submit", function (event) { 
+const login = document.getElementById('login'); // guardamos el form en login
+
+async function call_users() {
+    try{
+    const response = await fetch('./users.json');
+    const data = await response.json()
+    return data;
+    } catch(error){
+        console.log(`error: ${error}`);
+    }
+}
+
+
+
+
+
+
+
+login.addEventListener("submit", async function (event) { 
     event.preventDefault();
-    const user = document.getElementById("in_user");
-    const pass = document.getElementById("in_pass");
-    if (user.value === "admin" && pass.value === "123") {
-        window.location.href = "/inicio.html";
-        sessionStorage.setItem('log', 'true')
-    } 
-    if(user.value != "admin"){ 
-        user.classList.add("is-invalid");
-    }
-    if (pass.value != "123"){
-      pass.classList.add("is-invalid");
-    }
+        const users = await call_users();
+        const user_name = document.getElementById("in_user").value;
+        const user_pass = document.getElementById("in_pass").value;
+
+        const find_user = users.find(u => u.user === user_name && u.pass === user_pass);
+        if(find_user){
+            window.location.href = '/inicio.html';
+            sessionStorage.setItem('log', 'true')  
+        } else {
+            Toastify({
+
+                text: "error de datos",
+                
+                duration: 3000
+                
+                }).showToast();
+        }
 })
